425f4149fa1dbb1664e59a19c545b93cf885eea3
LupusVII <jln.thirion@gmail.com>
2019-02-02 12:26:23 +0100

    Add a title to the grid preview modal

--- src/Resources/public/js/backend.js
+++ src/Resources/public/js/backend.js
@@ -10,18 +10,20 @@ window.addEvent("domready", function() {
 		i.addEventListener("click", function(e){
 			e.preventDefault();
 			var w = '100%';
+			var title = "Grid Preview : ";
 			switch(e.target.getAttribute('data-breakpoint')){
-				case 'xxs': w = '520px'; break;
-				case 'xs': w = '620px'; break;
-				case 'sm': w = '768px'; break;
-				case 'md': w = '992px'; break;
-				case 'lg': w = '1200px'; break;
-				case 'xl': w = '1400px'; break;
+				case 'xxs': w = '520px'; bounds = "XXS (From 0px to 520px)"; break;
+				case 'xs': w = '620px'; bounds = "XS (From 521px to 620px)"; break;
+				case 'sm': w = '768px'; bounds = "SM (From 621px to 768px)"; break;
+				case 'md': w = '992px'; bounds = "MD (From 769px to 992px)"; break;
+				case 'lg': w = '1200px'; bounds = "LG (From 993px to 1200px)"; break;
+				case 'xl': w = '1400px'; bounds = "XL (From 1201px to 1400px)"; break;
 				default: w = '100%';
 			}
 
 			Backend.openModalIframe({
 				width:w
+				,title:title+bounds
 				,url:window.location.href+'&grid_preview=1'
 			});
 		});
