e1819d829257336d0c2c2baaa4a17e753e53cd89
ChipsVII <benjamin@webexmachina.fr>
2023-05-12 18:26:52 +0200

    Fix : use class  for fake items

--- src/Helper/GridBuilder.php
+++ src/Helper/GridBuilder.php
@@ -152,7 +152,7 @@ class GridBuilder
 
         $grid = $gop->getGridById($gridId);
 
-        $additionnalCssClasses = \WEM\GridBundle\Elements\GridStart::MODE_AUTOMATIC === $grid->getMode() ? 'cols-span-3' : str_replace('cols-', 'cols-span-', implode(' ', $grid->getWrapperClasses()));
+        $additionnalCssClasses = \WEM\GridBundle\Elements\GridStart::MODE_AUTOMATIC === $grid->getMode() ? 'cols-span-all' : str_replace('cols-', 'cols-span-', implode(' ', $grid->getWrapperClasses()));
 
         return sprintf('<div class="item-grid be_item_grid fake-helper be_item_grid_fake %s" dropable="true" draggable="false" data-type="fake-first-element">%s</div>', $additionnalCssClasses, $GLOBALS['TL_LANG']['WEM']['GRID']['BE']['placeToGridStart']);
     }
@@ -166,7 +166,7 @@ class GridBuilder
 
         $grid = $gop->getGridById($gridId);
 
-        $additionnalCssClasses = \WEM\GridBundle\Elements\GridStart::MODE_AUTOMATIC === $grid->getMode() ? 'cols-span-3' : '';
+        $additionnalCssClasses = \WEM\GridBundle\Elements\GridStart::MODE_AUTOMATIC === $grid->getMode() ? 'cols-span-all' : '';
 
         return sprintf('<div class="item-grid be_item_grid fake-helper be_item_grid_fake %s" dropable="true" draggable="false" data-type="fake-last-element">%s</div>', $additionnalCssClasses, $GLOBALS['TL_LANG']['WEM']['GRID']['BE']['placeToGridEnd']);
     }
@@ -180,8 +180,8 @@ class GridBuilder
 
         $grid = $gop->getGridById($gridId);
 
-        $additionnalCssClasses = \WEM\GridBundle\Elements\GridStart::MODE_AUTOMATIC === $grid->getMode() ? 'cols-span-3' :'';
+        $additionnalCssClasses = \WEM\GridBundle\Elements\GridStart::MODE_AUTOMATIC === $grid->getMode() ? 'cols-span-all' : '';
 
-        return sprintf('<div class="item-grid be_item_grid fake-helper be_item_grid_fake %s" dropable="false" draggable="false"><div class="item-new"></div></div>',$additionnalCssClasses);
+        return sprintf('<div class="item-grid be_item_grid fake-helper be_item_grid_fake %s" dropable="false" draggable="false"><div class="item-new"></div></div>', $additionnalCssClasses);
     }
 }
